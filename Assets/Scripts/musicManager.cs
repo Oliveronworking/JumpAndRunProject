ddbdc5e311c8c51d4617e5e57056bab04f4cba85
DragonBossHD <noam.mangold@gmail.com>
2024-06-13 14:09:16 +0200

    save music volume between scenes

--- Assets/Scripts/musicManager.cs
+++ Assets/Scripts/musicManager.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class musicManager : MonoBehaviour
 {
     public AudioSource audioSource;
     private float musicVolume = 1f;
+    public Slider volumeSlider;
     // Start is called before the first frame update
     void Start()
     {
         //audioSource.Play();
+        musicVolume = PlayerPrefs.GetFloat("volume");
+        audioSource.volume = musicVolume;
+        volumeSlider.value = musicVolume;
     }
 
     // Update is called once per frame
     void Update()
     {
         audioSource.volume = musicVolume;
+        PlayerPrefs.SetFloat("volume", musicVolume);
     }
     public void UpdateVolume(float volume)
     {
