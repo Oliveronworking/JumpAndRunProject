d50fbc1de9bd5c97a8f063fd6076cf196642ffb9
Maid2106 <maid.dzafic@icloud.com>
2024-06-19 12:14:55 +0200

    Sliden/Rutschen
    Rutscht nach Links aktuell wenn man Shift Taste betätigt

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,10 +13,13 @@ public class PlayerController : MonoBehaviour
     public AudioClip jumpSound;
     public AudioClip crashSound;
     public float jumpForce;
+    public float slideSpeed = 40f;  // Geschwindigkeit des Rutschens
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
     public GameObject GameOverCanvas;
+    private bool isSliding = false;  // Überprüfen, ob der Charakter rutscht
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,22 +32,48 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Springen
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
-    {
-        playerRb.AddForce(Vector3.up* jumpForce, ForceMode.Impulse);
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
             playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
-}
+
+        // Rutschen
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isOnGround && !isSliding && !gameOver)
+        {
+            StartCoroutine(SlideDown());
+        }
+    }
+
+    private IEnumerator SlideDown()
+    {
+        isSliding = true;
+        playerAnim.SetBool("Slide_b", true);  // Assuming you have a slide animation
+        float slideTime = 1f;  // Dauer des Rutschens
+        float startTime = Time.time;
+
+        while (Time.time < startTime + slideTime)
+        {
+            playerRb.AddForce(Vector3.forward * slideSpeed, ForceMode.Impulse);
+            yield return null;
+        }
+
+        playerAnim.SetBool("Slide_b", false);
+        isSliding = false;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-
-        if (collision.gameObject.CompareTag("Ground")) {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
             isOnGround = true;
             dirtParticle.Play();
-        } else if(collision.gameObject.CompareTag("Obstacle"))
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
@@ -54,5 +83,5 @@ public class PlayerController : MonoBehaviour
             playerAudio.PlayOneShot(crashSound, 1.0f);
             GameOverCanvas.SetActive(true);
         }
-    } 
+    }
 }
