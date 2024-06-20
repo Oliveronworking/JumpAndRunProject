091dd14eecd0c9739c48450045dd0c12ddae3a63
Oliveronworking <112431433+Oliveronworking@users.noreply.github.com>
2024-06-20 12:28:46 +0200

    charactercanvasweaponcanvas

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -10,24 +10,11 @@ public class Menu : MonoBehaviour
     public GameObject mainCanvas;
     public GameObject charactersCanvas;
     public GameObject Player;
-    public GameObject weaponCreateObject; // Referenz zum GameObject, das das createweapon-Skript enthält
-
-    public Button weaponSelectButton; // Referenz zum neuen Button
 
     // Start is called before the first frame update
     void Start()
     {
         Player.SetActive(true);
-
-        // Initialisiere den WeaponSelectButton
-        if (weaponSelectButton != null)
-        {
-            weaponSelectButton.onClick.AddListener(OnWeaponSelectButtonClick);
-        }
-        else
-        {
-            Debug.LogError("Weapon Select Button is not assigned.");
-        }
     }
 
     public void OnPlayButton()
@@ -49,37 +36,15 @@ public class Menu : MonoBehaviour
         Player.SetActive(false);
     }
 
-    public void OnCharacktersButton()
+    public void OnCharactersButton()
     {
         charactersCanvas.SetActive(true);
         mainCanvas.SetActive(false);
     }
 
-    void OnWeaponSelectButtonClick()
+    public void OnWeaponSelectButtonClick()
     {
-        if (weaponCreateObject != null)
-        {
-            // Deaktiviere die Charakterauswahl und andere Canvas
-            charactersCanvas.SetActive(false);
-            mainCanvas.SetActive(false);
-
-            // Zeige den Spieler und die Waffe an
-            Player.SetActive(true);
-
-            createweapon weaponCreateComponent = weaponCreateObject.GetComponent<createweapon>();
-
-            if (weaponCreateComponent != null)
-            {
-                weaponCreateComponent.createWeapon();
-            }
-            else
-            {
-                Debug.LogError("createweapon component not found on the assigned GameObject.");
-            }
-        }
-        else
-        {
-            Debug.LogError("weaponCreateObject is not assigned.");
-        }
+        mainCanvas.SetActive(false);
+        Player.SetActive(false);
     }
 }
